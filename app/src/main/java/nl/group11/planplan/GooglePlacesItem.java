11636854855497ed8844ab8020448ef545d3b91b
jamiro24 <jamiroleander24@gmail.com>
2016-03-08 09:39:56 +0100

    fixed the project not compiling

--- app/src/main/java/nl/group11/planplan/GooglePlacesItem.java
+++ app/src/main/java/nl/group11/planplan/GooglePlacesItem.java
@@ -1,14 +1,20 @@
 package nl.group11.planplan;
 
+import android.content.Context;
 import android.view.View;
 
 import com.google.android.gms.location.places.Place;
+
+import java.util.Date;
 
 /**
  * Created by s140442 on 07/03/2016.
  */
 public class GooglePlacesItem extends Item {
 
+    public GooglePlacesItem(Context c) {
+        super(c);
+    }
     Place place;
 
     @Override
@@ -17,7 +23,57 @@ public class GooglePlacesItem extends Item {
     }
 
     @Override
-    public Float extractPrice() {
+    public String getPrice() {
+        return null;
+    }
+
+    @Override
+    public Date getStartTime() {
+        return null;
+    }
+
+    @Override
+    public Date getUserStartTime() {
+        return null;
+    }
+
+    @Override
+    public Date getEndTime() {
+        return null;
+    }
+
+    @Override
+    public Date getUserEndTime() {
+        return null;
+    }
+
+    @Override
+    public String getImage() {
+        return null;
+    }
+
+    @Override
+    public String getID() {
+        return null;
+    }
+
+    @Override
+    public Enum getType() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public String getAddress() {
         return null;
     }
 
