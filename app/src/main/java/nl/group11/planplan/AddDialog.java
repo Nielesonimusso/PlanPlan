f652c669b3be001addf7f97967d9a3a75f6d61f1
Anne Kok <annelindakok@gmail.com>
2016-03-22 11:07:49 +0100

    Stub for add dialog

---
+++ app/src/main/java/nl/group11/planplan/AddDialog.java
@@ -0,0 +1,18 @@
+package nl.group11.planplan;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Anne on 22/03/2016.
+ */
+public class AddDialog extends DialogFragment {
+
+}
