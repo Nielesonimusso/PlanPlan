d6675fb4b21fe07de24c290f104a68c7298711da
Anne Kok <annelindakok@gmail.com>
2016-03-29 10:29:15 +0200

    Fix van Koen voor leading zeroes

--- app/src/main/java/nl/group11/planplan/TimePickerDialog.java
+++ app/src/main/java/nl/group11/planplan/TimePickerDialog.java
@@ -8,9 +8,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.TimePicker;
-
 
 public class TimePickerDialog extends Activity {
 
@@ -24,11 +22,10 @@ public class TimePickerDialog extends Activity {
         setContentView(R.layout.dialog_timepicker);
 
         final TimePicker tp = (TimePicker) findViewById(R.id.timePicker1);
+        // Setting the 24 hour view for the timePicker
         tp.setIs24HourView(true);
 
-        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        // setSupportActionBar(toolbar);
-
+        // When the cancel button is clicked the dialog should close
         final Button cancelButton = (Button) findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(
                 new View.OnClickListener() {
@@ -37,22 +34,40 @@ public class TimePickerDialog extends Activity {
                     }
                 });
 
-        // We listen if the setTimeButton is clicked, if it is clicked we save the entered time.
+        // We listen if the setTimeButton is clicked. if it is clicked we save the entered time.
         final Button setTimeButton = (Button) findViewById(R.id.setTimeButton);
         setTimeButton.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {
+                        // Two variables that correspond to the device time in hours and minutes
                         hour = tp.getCurrentHour();
                         min = tp.getCurrentMinute();
-                        //TextView time = (TextView) findViewById(R.id.startTime);//niet textview2 maar iets op dialog_add
-                        if (changeStartTime) {
-                            AddDialog.startTime.setText(new StringBuilder().append(hour).append(":").append(min)); //niet mainview maar dialog_add
+                        // If we change the start time we want to add it to the start time textField
+                        if(changeStartTime) {
+                            // A bunch of if statements to ensure the right time format is used
+                            if (hour < 10 && min < 10) {
+                                AddDialog.startTime.setText(new StringBuilder().append("0" + hour).append(":").append("0" + min));
+                            } else if (hour < 10 && min >= 10) {
+                                AddDialog.startTime.setText(new StringBuilder().append("0" + hour).append(":").append(min));
+                            } else if (hour >= 10 && min < 10) {
+                                AddDialog.startTime.setText(new StringBuilder().append(hour).append(":").append("0" + min));
+                            } else {
+                                AddDialog.startTime.setText(new StringBuilder().append(hour).append(":").append(min));
+                            }
                         } else {
-                            AddDialog.endTime.setText(new StringBuilder().append(hour).append(":").append(min));
+                            // A bunch of if statements to ensure the right time format is used
+                            if (hour < 10 && min < 10) {
+                                AddDialog.endTime.setText(new StringBuilder().append("0" + hour).append(":").append("0" + min));
+                            } else if (hour < 10 && min >= 10) {
+                                AddDialog.endTime.setText(new StringBuilder().append("0" + hour).append(":").append(min));
+                            } else if (hour >= 10 && min < 10) {
+                                AddDialog.endTime.setText(new StringBuilder().append(hour).append(":").append("0" + min));
+                            } else {
+                                AddDialog.endTime.setText(new StringBuilder().append(hour).append(":").append(min));
+                            }
                         }
                         finish();
                     }
                 });
     }
-
 }
